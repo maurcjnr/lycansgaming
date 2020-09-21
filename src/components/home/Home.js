f4997b1f546c1b0b3fc388a59e95910e1edd2559
Maurício Júnior <mauriciojunior.dev@gmail.com>
2020-09-21 01:26:54 -0300

    Iniciando projeto

---
+++ src/components/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default props => {
+  return (
+    <div className="Home">
+      <div className="ComingSoon">Em breve...</div>
+      <div className="Image"></div>
+      <div className="Loader"></div>
+    </div >
+  )
+}
