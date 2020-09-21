2b5bd9d09dc2817db51ada0c18192696a6d205d5
Maurício Júnior <mauriciojunior.dev@gmail.com>
2020-09-21 11:47:54 -0300

    Adicionado botões de social media

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 export default props => {
   return (
-    <div className="Home">
-      <div className="ComingSoon">Em breve...</div>
-      <div className="Image"></div>
-      <div className="Loader"></div>
+    <div className="home">
+      <div className="home_coming-soon">Novidades em breve...</div>
+      <div className="home_image"></div>
+      <div className="home_loader"></div>
+      <div className="home_social">
+        <a className="home_social_facebook" href="https://pt-br.facebook.com/" target="_blank"></a>
+        <a className="home_social_instagram" href="https://www.instagram.com/lyc.oficial" target="_blank"></a>
+        <a className="home_social_twitter" href="https://www.twitter.com/lycansoficial" target="_blank"></a>
+      </div>
     </div >
   )
 }
