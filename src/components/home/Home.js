e1af2f0ccdadc74d3ff8153f1d6c25bd9ebf2bb5
Maurício Júnior <mauriciojunior.dev@gmail.com>
2020-09-22 11:49:13 -0300

    Adiciona efeito nos botôes de social media e modificado keywords e description do index.html

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,13 +3,17 @@ import React from 'react'
 export default props => {
   return (
     <div className="home">
-      <div className="home_coming-soon">Novidades em breve...</div>
-      <div className="home_image"></div>
-      <div className="home_loader"></div>
-      <div className="home_social">
-        <a className="home_social_facebook" href="https://pt-br.facebook.com/" target="_blank"></a>
-        <a className="home_social_instagram" href="https://www.instagram.com/lyc.oficial" target="_blank"></a>
-        <a className="home_social_twitter" href="https://www.twitter.com/lycansoficial" target="_blank"></a>
+      <div className="home__coming-soon">Novidades em breve...</div>
+      <div className="home__image"></div>
+      <div className="home__loader"></div>
+      <div className="home__social">
+        <a className="home__social__instagram" href="https://www.instagram.com/lyc.oficial" target="_blank"></a>
+        <a className="home__social__twitch" href="https://www.twitch.tv/maurcjnr" target="_blank"></a>
+        <a className="home__social__twitter" href="https://www.twitter.com/lycansoficial" target="_blank"></a>
+      </div>
+      <div className="home__contact">
+        <p>E-mail para contato:</p>
+        <p>contato@lycansgaming.com.br</p>
       </div>
     </div >
   )
